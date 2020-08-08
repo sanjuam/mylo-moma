1c5fa32fa1eeb7f4e52c00bc7c596059bf752446
e5606756 <Marvin.SanJuan@fisglobal.com>
2020-08-08 13:00:51 +0800

    initial commit

---
+++ src/main/java/oso/org/mylomoma/MyloMomaApplication.java
@@ -0,0 +1,13 @@
+package oso.org.mylomoma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyloMomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyloMomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oso/org/mylomoma/MyloMomaApplicationTests.java
@@ -0,0 +1,13 @@
+package oso.org.mylomoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyloMomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
